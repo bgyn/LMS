0d4c3a5993f3825ddfbee62efc72d2556d5fdad7
Bigyan Thanait <bigyanthanait8@gmail.com>
2025-02-07 17:11:04 +0545

    fix: removed the params in GetProfile usecase

--- lib/client/feature/profile/domain/usecase/get_profile.dart
+++ lib/client/feature/profile/domain/usecase/get_profile.dart
@@ -1,16 +1,14 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:lms/client/core/faliure/faliure.dart';
-import 'package:lms/client/core/params/update_profile_params.dart';
 import 'package:lms/client/core/usecase/usecase.dart';
 import 'package:lms/client/feature/profile/domain/model/profile_model.dart';
 import 'package:lms/client/feature/profile/domain/repository/profile_repository.dart';
 
-class GetProfile extends UseCase<ProfileModel,UpdateProfileParams>{
+class GetProfile extends UseCase<ProfileModel, void> {
   final ProfileRepository _repository;
   GetProfile(this._repository);
   @override
   Future<Either<Failure, ProfileModel>> call(void params) {
     return _repository.getProfile();
   }
-  
 }
